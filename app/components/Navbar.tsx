37ebeda8900768ae95dfd735b9f7e9ab1cf4e183
JeremyTjahjana <jeremytjahjana@gmail.com>
2026-04-21 11:36:52 +0700

    fix navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
           <li>
             <a
               href="#"
-              className="flex items-center gap-2 text-[var(--biru)] border-b-2 border-[var(--biru)] pb-1 font-semibold"
+              className="flex items-center gap-2 hover:text-[var(--biru)] transition-color"
             >
               <Image src={assets.lightbulb} alt="Tutor" className="w-5 h-5" />
               <span>Tutor</span>
